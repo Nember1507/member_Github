0dc8014cee35cde6fa51ef98a94d6dad94ac278d
Nember <efremovzhenia2104@gmail.com>
2021-11-06 11:30:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/member_github/Contributor.java
@@ -0,0 +1,32 @@
+package com.example.member_github;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public class Contributor {
+
+    private String login;
+    private int contributions;
+
+    @Override
+    public String toString(){
+        return login + " (" + contributions + ")";
+    }
+}
+
+interface GitHubService {
+    @GET("repos/{owner}/{repo}/contributors")
+    Call<List<Contributor>> repoContributors(
+            @Path("owner") String owner,
+            @Path("repo") String repo);
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+}
